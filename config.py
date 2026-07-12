16269ce9f3b2f63e6c4da83cbe149b0575b75818
swaathihaa <swaathihaa@gmail.com>
2026-07-12 13:11:05 +0000

    Add EMBED_MODEL

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 EMAIL = "22f3002344@ds.study.iitm.ac.in"
 AIPIPE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6IjIyZjMwMDIzNDRAZHMuc3R1ZHkuaWl0bS5hYy5pbiIsImlhdCI6MTc4Mzg0MTQyNCwiaXNzIjoiaHR0cHM6Ly9haXBpcGUub3JnIiwiYXVkIjoiYWlwaXBlLWFwaSIsImV4cCI6MTc4NDQ0NjIyNH0.miHryJ44vzOgZV5_jU0eCj18vie7pVA1SJq4-L27KN8"
 AIPIPE_BASE = "https://aipipe.org/openai/v1"
+EMBED_MODEL = "text-embedding-3-small"
